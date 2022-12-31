70083b3bb02d842e8637af444900ff94bfba9e50
Parth-Hariyani3001 <hariyaniparth-inft@atharvacoe.ac.in>
2023-01-01 03:50:17 +0530

    Database Update

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -5,12 +5,12 @@ import 'firebase/firestore';
 
 // Use your own configs!
 const app = firebase.initializeApp({
-  apiKey: "AIzaSyA5ah3RMxF1Vn0uIir09_ff1ulbeMnIAxU",
-  authDomain: "auctioneer-53c42.firebaseapp.com",
-  projectId: "auctioneer-53c42",
-  storageBucket: "auctioneer-53c42.appspot.com",
-  messagingSenderId: "792769543898",
-  appId: "1:792769543898:web:1ff8716270600d63a72fe3"
+  apiKey: "AIzaSyALdREMorCck6QdSDKBz51OMsPGKOEOeHE",
+  authDomain: "auctioneer-cfadb.firebaseapp.com",
+  projectId: "auctioneer-cfadb",
+  storageBucket: "auctioneer-cfadb.appspot.com",
+  messagingSenderId: "652989739042",
+  appId: "1:652989739042:web:e2c4179e12e57902bc1a7b"
 });
 
 export const timestamp = firebase.firestore.FieldValue.serverTimestamp;
